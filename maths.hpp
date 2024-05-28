57ab007a803d9de7affb29e4b0bd17fe8844979b
Promswag <promswag@gmail.com>
2024-05-28 18:11:13 +0200

    angle_cos, cross_product

--- maths.hpp
+++ maths.hpp
@@ -17,4 +17,21 @@ auto lerp(K u, K v, float t) {
 	return K(u + (v - u) * t);
 }
 
+template<typename K>
+auto angle_cos(Vector<K> u, Vector<K> v) {
+	return float(u.dot(v) / (u.norm() * v.norm()));
+}
+
+template<typename K>
+auto cross_product(Vector<K> u, Vector<K> v) {
+	if (u.size() != 3 || v.size() != 3)
+		throw VectorException("Both vectors shall be 3D");
+	Vector<K> result(3);
+	result[0] = u[1] * v[2] - u[2] * v[1];
+	result[1] = u[2] * v[0] - u[0] * v[2];
+	result[2] = u[0] * v[1] - u[1] * v[0];
+	return result;
+}
+
+
 #endif
